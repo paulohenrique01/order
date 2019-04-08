7d152579bcecda3cec271d72f30ac6d323bce676
Paulo Martins <paulohenrique.sistemas@gmail.com>
2019-04-08 09:03:53 -0300

    Iniciando componente de navegação

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
+
+
 class App extends Component {
-  render() {
-    return (
-      <div >
-        <p>Hello world</p>
-      </div>
+
+ 
+  render() {  
+   
+    return (      
+     <AppBar position="static">
+      <Toolbar>
+        <IconButton  color="inherit" aria-label="Menu">
+          <MenuIcon />
+        </IconButton>
+        <Typography variant="h6" color="inherit" >
+          News
+        </Typography>
+        <Button color="inherit">Login</Button>
+      </Toolbar>
+    </AppBar>
+   
     );
   }
 }
+
+App.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
 
 export default App;
