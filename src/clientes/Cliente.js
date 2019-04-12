d0c6428fe15823c7f271f48689fe58d7979d8c3e
Paulo Martins <paulohenrique.sistemas@gmail.com>
2019-04-11 23:20:10 -0300

    Início da criação do filter do formulário de cliente

--- src/clientes/Cliente.js
+++ src/clientes/Cliente.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
+import ClienteFilter from './ClienteFilter';
 
 class Cliente extends Component {
 
- 
-  render() {  
-   
-    return (      
-     <div>
-       <p>Cadastro de cientes</p>
+  render() {
 
-     </div>
-             
-   
+    return (
+      <div>
+        <p>Cadastro de clientes</p>
+        <ClienteFilter />
+      </div>
+
+
     );
   }
 }
